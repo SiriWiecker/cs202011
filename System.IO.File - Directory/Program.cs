4dfbf93b8e34bd9144326c78eddaaf264cfa9f76
Admin <Admin@TA-826-08>
2020-12-01 11:03:25 +0100

    Frokost dag 6

---
+++ System.IO.File - Directory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace System.IO.File___Directory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.Exists(@"c:\tmp"));
+            Console.WriteLine(Directory.GetFiles(@"c:\tmp"));
+            
+        }
+    }
+}
